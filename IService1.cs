16086da3b60d76d242dfdb97f323b4c8d5059e4a
falkonxd <jhonly_thebest1@hotmail.com>
2016-07-02 05:38:23 -0500

    f
    g

---
+++ IService1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WcfService1.DTO;
+
+namespace WcfService1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        List<DTO.ProductoDTO> ProductosDisponibles();
+
+        [OperationContract]
+        List<DTO.CategoriaDTO> Categoriagetall();
+
+        [OperationContract]
+        List<DTO.ProductoDTO> ProductosporCategoria(string categoryid);
+
+        [OperationContract]
+        List<DTO.ProductoDTO> ProductosporMarca(string marcaid);
+
+        [OperationContract]
+        List<DTO.ProductoDTO> Productogetall();
+
+        [OperationContract]
+        List<DTO.MarketingDTO> Marketinggetall();
+
+        [OperationContract]
+        DTO.ProductoDTO getProductobyID(string productoid);
+
+        [OperationContract]
+        List<DTO.ProductoDTO> getProductosFavoritos();
+
+        [OperationContract]
+        List<DTO.CategoriaDTO> getCategoriesTipo(string dato);
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WcfService1.DTO;
+using AutoMapper;
+using Dominio;
+
+namespace WcfService1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public List<CategoriaDTO> Categoriagetall()
+        {
+            Categoria categoriaDominio = new Categoria();
+            var categorias = categoriaDominio.TraerTodo();
+
+            var coleccion = Mapper.Map<List<DTO.CategoriaDTO>>(categorias);
+
+            return coleccion;
+        }
+
+        public List<CategoriaDTO> getCategoriesTipo(string dato)
+        {
+            Categoria categoria = new Categoria();
+            var categorias = categoria.Buscar(c => c.Tipo == dato);
+
+            var coleccion = Mapper.Map<List<DTO.CategoriaDTO>>(categorias);
+
+            return coleccion;
+        }
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public ProductoDTO getProductobyID(string productoid)
+        {
+            Producto producto = new Producto();
+            var productos = producto.TraerTodoPorID(productoid);
+
+            var objeto = Mapper.Map<DTO.ProductoDTO>(productos);
+            return objeto;
+        }
+
+        public List<ProductoDTO> getProductosFavoritos()
+        {
+            Producto producto = new Producto();
+            var productos = producto.Buscar(p => p.IsFavorite == true);
+            var coleccion = Mapper.Map<List<DTO.ProductoDTO>>(productos);
+
+            return coleccion;
+        }
+
+        public List<MarketingDTO> Marketinggetall()
+        {
+            Marketing marketingDominio = new Marketing();
+            var marketings = marketingDominio.TraerTodo();
+
+            var coleccion = Mapper.Map<List<DTO.MarketingDTO>>(marketings);
+            return coleccion;
+        }
+
+        public List<ProductoDTO> Productogetall()
+        {
+            Producto productoDominio = new Producto();
+            var productos = productoDominio.TraerTodo();
+
+            var coleccion = Mapper.Map<List<DTO.ProductoDTO>>(productos);
+            return coleccion;
+        }
+
+        public List<ProductoDTO> ProductosDisponibles()
+        {
+            Producto producto = new Producto();
+            var productos = producto.Buscar(p => p.UnidadesStock > 0);
+            var coleccion = Mapper.Map<List<DTO.ProductoDTO>>(productos);
+
+            return coleccion;
+        }
+
+        public List<ProductoDTO> ProductosporCategoria(string categoryid)
+        {
+            Producto producto = new Producto();
+            var productos = producto.Buscar(p => p.Categoria == categoryid);
+            var coleccion = Mapper.Map<List<DTO.ProductoDTO>>(productos);
+
+            return coleccion;
+        }
+
+        public List<ProductoDTO> ProductosporMarca(string marcaid)
+        {
+            Producto producto = new Producto();
+            var productos = producto.Buscar(p => p.Marca == marcaid);
+            var coleccion = Mapper.Map<List<DTO.ProductoDTO>>(productos);
+
+            return coleccion;
+        }
+    }
+}
